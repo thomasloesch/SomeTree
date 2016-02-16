13521fad782a89af470308e6bf03cfb843bbcc77
thomasloesch <tloesch@bgsu.edu>
2016-02-16 12:07:12 -0500

    Changed from an array of int to an array of points. Also modified if/else to properly respond to changes in slope.

--- ProgrammingAssignment2/main.cpp
+++ ProgrammingAssignment2/main.cpp
@@ -32,7 +32,7 @@ typedef struct pt2d
 void display_func(void);
 void keyboard_func(unsigned char c, int x, int y);
 void mouse_func(int button, int state, int x, int y);
-void loeschLineScan(int x1, int y1, int x2, int y2);
+void loeschLineScan(My2DPoint p1, My2DPoint p2);
 void drawMegaPixel(int x, int y);
 void swapInts(int &a, int &b);
 
@@ -41,8 +41,7 @@ void swapInts(int &a, int &b);
 // Global Variables
 int _x = 300;
 int _y = 300;
-int xAry[44];
-int yAry[44];
+My2DPoint pointAry[22];
 
 //@@***********************************************************************************@@
 int main(int argc, char **argv)
@@ -51,10 +50,10 @@ int main(int argc, char **argv)
 
 	init_setup(WINDOW_XS, WINDOW_YS, WINDOW_NAME);
 	
-	xAry[0] = 175;
-	xAry[1] = 425;
-	yAry[0] = 175;
-	yAry[1] = 425;
+	pointAry[0].x = 175;
+	pointAry[0].y = 175;
+	pointAry[1].x = 425;
+	pointAry[1].y = 425;
 	
 	glutDisplayFunc(display_func);			// call back for display event
 	glutKeyboardFunc(keyboard_func);		// call back for keyboard event
@@ -74,7 +73,7 @@ void display_func(void)
 	glColor3f(1.0, 1.0, 1.0);						// set color (grey)
 	glLineWidth(1.0);
 
-	loeschLineScan(xAry[1], yAry[1], xAry[0], yAry[0]);
+	loeschLineScan(pointAry[0], pointAry[1]);
 	drawMegaPixel(200, 400);
 
 	glColor3f(0.0, 0.0, 0.0);		// set color (black)
@@ -124,21 +123,22 @@ void mouse_func(int button, int state, int x, int y)
 }	// end of mouse_func()
 
 // This is the Bresenham-based linescan function (DOES NOT use mega pixels)
-void loeschLineScan(int x1, int y1, int x2, int y2) {
-	if (x1 > x2) {
-		swapInts(x1, x2);
-		swapInts(y1, y2);
+void loeschLineScan(My2DPoint p1, My2DPoint p2) {
+	if (p1.x > p2.x) {
+		swapInts(p1.x, p2.x); // change to swapPoints func
+		swapInts(p1.y, p2.y);
 	}
 
-	int dY = y2 - y1;
-	int dX = x2 - x1;
+	int dY = p2.y - p1.y;
+	int dX = p2.x - p1.x;
+	float m = (float)dY / (float)dX;
 	int dFa = 2 * (dY - dX);
 	int dFb = 2 * dY;
 	int p = 2 * dY - dX;
 	int currX = 0;
 	int currY = 0;
 
-	if(dY >= 0 && dY <= 1) { // standard case
+	if(m >= 0.0 && m <= 1.0) { // standard case
 		glBegin(GL_POINTS);
 		for (int k = 0; k < dX; k++) {
 			if (p < 0) 
@@ -147,14 +147,14 @@ void loeschLineScan(int x1, int y1, int x2, int y2) {
 				currY++;
 				p += dFa;
 			}
-			glVertex2i(x1 + k, y1 + currY);
+			glVertex2i(p1.x + k, p1.y + currY);
 		}
 		glEnd();
 	}
-	else if (dY > 1) { // slope greater than 1
+	else if (m > 1.0) { // slope greater than 1
 		
 	}
-	else if (dY < 0 && >= -1) { // negative slope
+	else if (m < 0.0 && m >= -1.0) { // negative slope
 		
 	}
 	else { // slope must be negative and less than -1
